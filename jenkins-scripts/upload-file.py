b1847e92f8e0612021ad3a55f3c6221cd766f373
Nahum Timerman <ntimerman@gmail.com>
2016-11-09 14:47:44 +0200

    changed upload file name

--- jenkins-scripts/upload-file.py
+++ jenkins-scripts/upload-file.py
@@ -1,15 +1,20 @@
 import requests
 import sys
 from uuid import UUID
+import os.path
 
-if len(sys.argv) == 1:
-    raise Exception('must pass in reservation_id')
+if len(sys.argv) < 3:
+    raise Exception('must pass in reservation_id and archive filepath')
 
 try:
     ReservationID = UUID(sys.argv[1], version=4)
 except Exception as e:
     raise Exception('first argument was not a reservation id')
 
+artifactory_metadata_file_path = sys.argv[2]
+
+if not os.path.isfile(artifactory_metadata_file_path):
+    raise Exception('second argument is file path, but file does not exist')
 
 serverAddress = 'localhost'
 quali_api_port = '9000'
@@ -22,10 +27,17 @@ creds = {"username": user, "password": password, "domain": domain}
 login_result = requests.put('http://{0}:{1}/Api/Auth/Login'.format(serverAddress, quali_api_port), creds)
 authcode = "Basic " + login_result.content[1:-1]
 
-with open("c:\\debug.txt", 'rb') as upload_file:
+with open(artifactory_metadata_file_path, 'rb') as upload_file:
     res = requests.post('http://{0}:{1}/API/Package/AttachFileToReservation'.format(serverAddress,
                                                                                     quali_api_port),
                         headers={"Authorization": authcode},
-                        data={"reservationId": ReservationID, "saveFileAs": "Build_Detail.pdf",
+                        data={"reservationId": ReservationID, "saveFileAs": "artifactory-metadata",
                               "overwriteIfExists": "True"},
                         files={'QualiPackage': upload_file})
+
+
+def download_from_artifactory(buildName, buildNumber):
+    artifactory_url = "http://localhost:41411/artifactory/api/archive/buildArtifacts?params=buildName={0}|buildNumber={1}|archiveType=zip".format(
+        buildName, buildNumber)
+    curl_string = 'curl -XPOST -u admin:password -H "Content-Type: application/json"  "{0}" > build.zip'.format(
+        artifactory_url)
