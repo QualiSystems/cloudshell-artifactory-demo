edc93ce56c3a1774b7059f8acb260d850cbb0d2d
Nahum Timerman <ntimerman@gmail.com>
2016-11-15 10:42:24 +0200

    zzz

--- jenkins-scripts/upload-file.py
+++ jenkins-scripts/upload-file.py
@@ -3,37 +3,39 @@ import sys
 from uuid import UUID
 import os.path
 
-if len(sys.argv) < 3:
-    raise Exception('must pass in reservation_id and archive filepath')
 
-try:
-    ReservationID = UUID(sys.argv[1], version=4)
-except Exception as e:
-    raise Exception('first argument was not a reservation id')
+def main():
+    if len(sys.argv) < 3:
+        raise Exception('must pass in reservation_id and archive filepath')
 
-artifactory_metadata_file_path = sys.argv[2]
+    try:
+        ReservationID = UUID(sys.argv[1], version=4)
+    except Exception as e:
+        raise Exception('first argument was not a reservation id')
 
-if not os.path.isfile(artifactory_metadata_file_path):
-    raise Exception('second argument is file path, but file does not exist')
+    artifactory_metadata_file_path = sys.argv[2]
 
-serverAddress = 'localhost'
-quali_api_port = '9000'
-user = 'admin'
-password = 'admin'
-domain = 'Global'
-creds = {"username": user, "password": password, "domain": domain}
+    if not os.path.isfile(artifactory_metadata_file_path):
+        raise Exception('second argument is file path, but file does not exist')
 
-# Login to Quali API
-login_result = requests.put('http://{0}:{1}/Api/Auth/Login'.format(serverAddress, quali_api_port), creds)
-authcode = "Basic " + login_result.content[1:-1]
+    serverAddress = 'localhost'
+    quali_api_port = '9000'
+    user = 'admin'
+    password = 'admin'
+    domain = 'Global'
+    creds = {"username": user, "password": password, "domain": domain}
 
-with open(artifactory_metadata_file_path, 'rb') as upload_file:
-    res = requests.post('http://{0}:{1}/API/Package/AttachFileToReservation'.format(serverAddress,
-                                                                                    quali_api_port),
-                        headers={"Authorization": authcode},
-                        data={"reservationId": ReservationID, "saveFileAs": "artifactory-metadata",
-                              "overwriteIfExists": "True"},
-                        files={'QualiPackage': upload_file})
+    # Login to Quali API
+    login_result = requests.put('http://{0}:{1}/Api/Auth/Login'.format(serverAddress, quali_api_port), creds)
+    authcode = "Basic " + login_result.content[1:-1]
+
+    with open(artifactory_metadata_file_path, 'rb') as upload_file:
+        res = requests.post('http://{0}:{1}/API/Package/AttachFileToReservation'.format(serverAddress,
+                                                                                        quali_api_port),
+                            headers={"Authorization": authcode},
+                            data={"reservationId": ReservationID, "saveFileAs": "artifactory-metadata",
+                                  "overwriteIfExists": "True"},
+                            files={'QualiPackage': upload_file})
 
 
 if __name__ == "__main__":
