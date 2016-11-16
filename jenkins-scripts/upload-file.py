3527a13d4600d7b701e36e2e8c9807e6e3f7dc11
Nahum Timerman <ntimerman@gmail.com>
2016-11-16 13:56:21 +0200

    pass server address in to upload file

--- jenkins-scripts/upload-file.py
+++ jenkins-scripts/upload-file.py
@@ -5,8 +5,8 @@ import os.path
 
 
 def main():
-    if len(sys.argv) < 3:
-        raise Exception('must pass in reservation_id and archive filepath')
+    if len(sys.argv) < 4:
+        raise Exception('must pass in reservation_id, archive filepath and cloudshell server host')
 
     try:
         ReservationID = UUID(sys.argv[1], version=4)
@@ -14,11 +14,11 @@ def main():
         raise Exception('first argument was not a reservation id')
 
     artifactory_metadata_file_path = sys.argv[2]
+    server_address = sys.argv[3]
 
     if not os.path.isfile(artifactory_metadata_file_path):
         raise Exception('second argument is file path, but file does not exist')
 
-    serverAddress = 'localhost'
     quali_api_port = '9000'
     user = 'admin'
     password = 'admin'
@@ -26,11 +26,11 @@ def main():
     creds = {"username": user, "password": password, "domain": domain}
 
     # Login to Quali API
-    login_result = requests.put('http://{0}:{1}/Api/Auth/Login'.format(serverAddress, quali_api_port), creds)
+    login_result = requests.put('http://{0}:{1}/Api/Auth/Login'.format(server_address, quali_api_port), creds)
     authcode = "Basic " + login_result.content[1:-1]
 
     with open(artifactory_metadata_file_path, 'rb') as upload_file:
-        res = requests.post('http://{0}:{1}/API/Package/AttachFileToReservation'.format(serverAddress,
+        res = requests.post('http://{0}:{1}/API/Package/AttachFileToReservation'.format(server_address,
                                                                                         quali_api_port),
                             headers={"Authorization": authcode},
                             data={"reservationId": ReservationID, "saveFileAs": "artifactory-metadata",
--- orchestration_scripts/import_files_from_artifactory.py
+++ orchestration_scripts/import_files_from_artifactory.py
@@ -3,6 +3,7 @@ import cloudshell.helpers.scripts.cloudshell_scripts_helpers as helpers
 import json
 import paramiko
 import os
+import time
 
 
 def scp_copy(host, user, password, local_path, remote_path):
@@ -17,15 +18,14 @@ def scp_copy(host, user, password, local_path, remote_path):
     ssh.close()
 
 
-
 class ArtifactBuild:
     def __init__(self, name, number):
         self._name = name
         self._number = number
 
-    def __init__(self, data):
-        self._name = data["Build Name"]
-        self._number = data["Build Number"]
+    @staticmethod
+    def from_dict(data):
+        return ArtifactBuild(data["Build Name"], data["Build Number"])
 
     @property
     def name(self):
@@ -46,11 +46,12 @@ class Artifactory:
         self._user = user
         self._password = password
 
-    def __init__(self, repo_details):
-        self._host = repo_details["Host"]
-        self._port = repo_details["Port"]
-        self._user = repo_details["User"]
-        self._password = repo_details["Password"]
+    @staticmethod
+    def from_dict(repo_details):
+        return Artifactory(repo_details["Host"],
+                           repo_details["Port"],
+                           repo_details["User"],
+                           repo_details["Password"])
 
     @property
     def host (self):
@@ -68,7 +69,10 @@ class Artifactory:
     def password(self):
         return self._password
 
-    def download(self, buildName, buildNumber, reservation_id):
+    def download(self, buildName, buildNumber):
+        file_name = "binaries_for_{0}_{1}.tar".format(buildName, buildNumber)
+        save_artifacts_archive_to = "c:\\demo"
+
         artifactory_url = "http://{0}:{1}/artifactory/api/archive/buildArtifacts".format(self.host, self.port)
         data = {
             "buildName": buildName.encode(),
@@ -77,14 +81,13 @@ class Artifactory:
         }
 
         headers = {'content-type': 'application/json'}
-        response = requests.post(artifactory_url, auth=(self.user, self.password), headers=headers, json=data)
 
-        file_name = "binaries_for_{0}_{1}.tar".format(buildName,buildNumber)
-        save_artifacts_archive_to = "c:\\demo"
-
-        zfile = open(os.path.join(save_artifacts_archive_to, file_name), 'wb')
-        zfile.write(response.content)
-        zfile.close()
+        with (open(os.path.join(save_artifacts_archive_to, file_name), 'wb')) as zfile:
+            response = requests.post(artifactory_url, auth=(self.user, self.password), headers=headers, json=data)
+            for chunk in response.iter_content():
+                if chunk:
+                    zfile.write(chunk)
+                    zfile.flush()
 
         del response
         return save_artifacts_archive_to, file_name
@@ -99,29 +102,36 @@ def mock_reservation():
     reservation = Object()
     reservation.id = '115b6d9f-f736-4682-bbc5-eba535520568'
 
-def populate_build_from_sandbox(build, connectivity, reservation):
+
+def populate_build_from_sandbox(connectivity, reservation, msg):
     serveraddress = connectivity.server_address
     quali_api_port = '9000'
     user = connectivity.admin_user
     password = connectivity.admin_pass
     domain = reservation.domain
-    print '- Checking if sandbox associated with a build'
+    msg('- Checking if sandbox associated with a build')
     creds = {"username": user, "password": password, "domain": domain}
     login_result = requests.put('http://{0}:{1}/Api/Auth/Login'.format(serveraddress, quali_api_port), creds)
     authcode = "Basic " + login_result.content[1:-1]
-    response = requests.post(
-        "http://{0}:{1}/API/Package/GetReservationAttachment".format(serveraddress, quali_api_port),
-        headers={"Authorization": authcode},
-        data={"reservationId": reservation.id,
-              "Filename": "artifactory-metadata"})
-    try:
-        metadata = json.loads(response.text.replace('/r/n', ''))
-    except Exception:
-        print '- App not associated with a build'
-    print '- found a build with artifacts hosted on Artifactory: {0} {1}'.format(metadata['buildName'],
-                                                                                 metadata['buildNumber'])
-    build = ArtifactBuild(metadata['buildName'], metadata['buildNumber'])
-    return build
+    for retry in xrange(5):
+        response = requests.post(
+            "http://{0}:{1}/API/Package/GetReservationAttachment".format(serveraddress, quali_api_port),
+            headers={"Authorization": authcode},
+            data={"reservationId": reservation.id,
+                  "Filename": "artifactory-metadata"})
+        try:
+            metadata = json.loads(response.text.replace('/r/n', ''))
+        except ValueError as ve:
+            time.sleep(15)
+        except Exception as e:
+            msg('- App not associated with a build')
+            msg(e.message)
+            raise e
+        else:
+            msg('- found a build with artifacts hosted on Artifactory: {0} {1}'.format(metadata['buildName'],
+                                                                                         metadata['buildNumber']))
+            build = ArtifactBuild(metadata['buildName'], metadata['buildNumber'])
+            return build
 
 def main():
     reservation = helpers.get_reservation_context_details()
@@ -129,36 +139,35 @@ def main():
     app_attributes = app.attributes
     connectivity = helpers.get_connectivity_context_details()
     cloudshell = helpers.get_api_session()
-
+    msg = lambda txt: cloudshell.WriteMessageToReservationOutput(reservation.id, txt)
 
     resource = helpers.get_resource_context_details_dict()
     resource['deployedAppData']['attributes'] = {attribute['name']: attribute['value'] for attribute in resource['deployedAppData']['attributes']}
     resource['deployedAppData']['attributes']['Password'] = cloudshell.DecryptPassword(resource['deployedAppData']['attributes']['Password']).Value
 
-    repo = cloudshell.GetResourceDetails(app_attributes["Artifactory Name"])
-    repo_attributes = {attribute.Name:attribute.Value for attribute in repo.ResourceAttributes}
+    repo = cloudshell.GetResourceDetails(app_attributes["Repository Name"])
+    repo_attributes = {attribute.Name: attribute.Value for attribute in repo.ResourceAttributes}
     repo_attributes["Host"] = repo.Address
-    # mock_reservation()
+    repo_attributes["Password"] = cloudshell.DecryptPassword(repo_attributes["Password"]).Value
 
-    artifactory = Artifactory(repo_attributes)
-    build = ArtifactBuild(repo_attributes)
+    artifactory = Artifactory.from_dict(repo_attributes)
+    build = ArtifactBuild.from_dict(repo_attributes)
 
-    print '- Looking for app dependencies'
+    msg('- Looking for app dependencies')
 
     if not build.is_populated():
-        build = populate_build_from_sandbox(build, connectivity, reservation)
+        build = populate_build_from_sandbox(connectivity, reservation, msg)
 
-    print '- Found dependencies on Artifactory, associated with build {0} {1}'.format(build.name, build.number)
+    msg('- Found dependencies on Artifactory, associated with build {0} {1}'.format(build.name, build.number))
 
-    file_location, file_name = artifactory.download(build.name, build.number, reservation.id)
+    file_location, file_name = artifactory.download(build.name, build.number)
 
-    print '- Downloaded app dependencies to ' + os.path.join(file_location, file_name)
+    msg('- Downloaded app dependencies to Execution Server at ' + os.path.join(file_location, file_name))
 
     scp_copy(resource['deployedAppData']['address'], resource['deployedAppData']['attributes']['User'], resource['deployedAppData']['attributes']['Password'],
              os.path.join(file_location, file_name), app_attributes['Target Directory'] + '/' + file_name)
 
-
-
+    msg('- Copied binaries to app server at ' + app_attributes['Target Directory'] + '/' + file_name)
 
 
 if __name__ == "__main__":
